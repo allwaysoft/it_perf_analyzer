b435ad79b17527f509c68b8153adcd9c40db0b27
Guy Fomi <guyfomi@gmail.com>
2017-07-11 18:25:57 +0100

    Bug in desktop resolved

--- admin/tocdesktop.php
+++ admin/tocdesktop.php
@@ -1,16 +1,4 @@
 <?php
-/*
-  $Id: account.php $
-  Mefobe Cart Solutions
-  http://www.mefobemarket.com
-
-  Copyright (c) 2009 Wuxi Elootec Technology Co., Ltd
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License v2 (1991)
-  as published by the Free Software Foundation.
-*/
-
 
   require_once('includes/application_top.php');
   
@@ -36,8 +24,6 @@ var token = '<?php echo $token ?>';
 Ext.Ajax.extraParams = {token: token};
 Ext.data.Connection.prototype.extraParams = {token: token};
 Ext.data.ScriptTagProxy.prototype.extraParams = {token: token};
-    
-var tocCurrenciesFormatter = Ext.util.Format.CurrencyFactory(parseInt('<?php echo $osC_Currencies->getDecimalPlaces(); ?>'), '<?php echo $osC_Language->getNumericDecimalSeparator(); ?>', '<?php echo $osC_Language->getNumericThousandsSeparator(); ?>', '<?php echo $osC_Currencies->getSymbolLeft(); ?>', '<?php echo $osC_Currencies->getSymbolRight(); ?>');
 
 /*
  * Desktop configuration
