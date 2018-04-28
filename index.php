f266bde614432b93157fd88bfee419ef0ff17258
guyfomi <guyfomi@gmail.com>
2018-04-28 21:18:41 +0100

    iframe index

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<?php
 
-header( 'Location: http://localhost/bi_server/admin');
-?>
+<!DOCTYPE html>
+<html>
+
+   <head>
+      <title>INNOVICS</title>
+   </head>
+	
+   <body>                 
+
+<iframe src="http://localhost/bi_server/admin" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+    Your browser doesn't support iframes
+</iframe>
+   </body>
+	
+</html>
