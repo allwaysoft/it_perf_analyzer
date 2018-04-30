dff7de5804672f886f5a3e0f947c7b726fffadc4
guyfomi <guyfomi@gmail.com>
2018-04-30 16:37:51 +0100

    metabase start script

--- metabase/start.sh
+++ metabase/start.sh
@@ -1,4 +1,10 @@
+echo "Demarrage BI Server ..."
 cd /opt/lampp/htdocs/bi_server/metabase
-sh kill_metabase.sh
 rm -f nohup.out
-nohup sudo java -Dh2.bindAddress=localhost -Xmx1024m -jar metabase.jar &
+
+export MB_PLUGINS_DIR=/opt/lampp/htdocs/bi_server/metabase/plugins
+
+export ram=`free -m | grep "^Mem"|awk '{print $2}'`
+export xmx=$(( $ram/2 ))
+
+nohup java -Xmx${xmx}m -jar metabase.jar &
