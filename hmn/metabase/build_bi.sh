0c2de8f7b3299b4b3cf66841162bf18579aee1d1
guyfomi <guyfomi@gmail.com>
2018-05-01 18:08:17 +0100

    mail error to log

--- hmn/metabase/build_bi.sh
+++
@@ -1,4 +0,0 @@
-sudo rm -f metabase.jar
-sudo cat metabase* >/tmp/metabase.jar
-sudo rm -f metabase*
-sudo cp /tmp/metabase.jar .
--- hmn/metabase/kill_metabase.sh
+++
@@ -1,2 +0,0 @@
-ps -ef|grep metabase|grep -v grep|awk '{print $2}'|xargs kill -9
-exit $?
